f6fa21b287fc28150d785160fac2c588ea014880
Echizen1605 <980561079@qq.com>
2019-03-18 20:10:09 +0800

    添加placeholder，添加qslider，添加即时刷新功能

--- auto_draw.py
+++ auto_draw.py
@@ -1,18 +1,40 @@
 # coding:utf-8
-
-from PyQt5 import QtWidgets, QtGui
-from PyQt5.QtWidgets import *
 import sys
-from PyQt5.QtCore import *
-import subprocess
-import os
 import numpy as np
+import matplotlib
 import matplotlib.pyplot as plt
+from PyQt5 import QtWidgets, QtGui
+from PyQt5.QtWidgets import QStatusBar, \
+							QLineEdit, \
+							QSlider,\
+							QHBoxLayout,\
+							QVBoxLayout,\
+							QGroupBox,\
+							QWidget,\
+							QApplication,\
+							QComboBox,\
+							QPushButton,\
+							QFormLayout,\
+							QMainWindow,\
+							QDesktopWidget,\
+							QColorDialog,\
+							QFileDialog,\
+							QAction,\
+							QStyle
+
+from PyQt5.QtCore import Qt, QEvent
+from PyQt5.QtGui import QFont
+
 
 # pyqt结合matplotlib的中介
 from matplotlib.backends.backend_qt5agg import FigureCanvasQTAgg as FigureCanvas
 from matplotlib.figure import Figure
 
+# matplotlib中文问题
+matplotlib.use('qt4agg')
+matplotlib.rcParams['font.sans-serif'] = ['SimHei']
+matplotlib.rcParams['font.family']='sans-serif'
+matplotlib.rcParams['axes.unicode_minus'] = False
 
 class Config(QWidget):
 	def __init__(self):
@@ -44,6 +66,7 @@ class Config(QWidget):
 		self.lc_list = []
 		self.lt_list = []
 		self.mark_list = []
+		self.mark_density_list = []
 
 		# 当前绘图曲线元数据保存
 		self.ls = '-'
@@ -51,14 +74,17 @@ class Config(QWidget):
 		self.formula = 'y=x**2'
 		self.lc = '#00ff00'
 		self.lt = 'Our Method'
+		self.md = 5
 
 		# 状态栏
 		self.statusBar = QStatusBar()
+		self.statusBar.setFont(QFont(u'华文楷体', 10))
 		self.statusBar.showMessage(u"准备就绪")
 
 		# 编辑公式
 		self.math = QLineEdit()
-		self.math.setText(u'输入线条公式')
+		self.math.setPlaceholderText(u'输入线条公式')
+		self.math.setClearButtonEnabled(True)
 
 		# 线条风格
 		self.linestyle = QComboBox()
@@ -75,7 +101,8 @@ class Config(QWidget):
 
 		# 设置线条标签名称
 		self.label = QLineEdit()
-		self.label.setText(u'Our Method')
+		self.label.setPlaceholderText(u'Our Method')
+		self.label.setClearButtonEnabled(True)
 
 		# 设置线条颜色
 		self.lineColor = QPushButton('更改线条颜色')
@@ -85,29 +112,42 @@ class Config(QWidget):
 		self.lineColor.resize(self.lineColor.sizeHint())
 		self.lineColor.installEventFilter(self)
 
+		# 设置标记密集程度
+		self.markDensity = QSlider(Qt.Horizontal)
+		self.markDensity.setMinimum(1)
+		self.markDensity.setMaximum(50)
+		self.markDensity.setValue(5)
+		self.markDensity.installEventFilter(self)
+
 		# 横坐标轴范围
 		self.x_scale = QLineEdit()
-		self.x_scale.setText(u'输入横坐标轴最小和最大范围，以逗号分隔，如：0,1')
+		self.x_scale.setPlaceholderText(u'输入横坐标轴最小和最大范围，以逗号分隔，如：0,1')
+		self.x_scale.setClearButtonEnabled(True)
 
 		# 纵坐标轴范围
 		self.y_scale = QLineEdit()
-		self.y_scale.setText(u'输入纵坐标轴最小和最大范围，以逗号分隔，如：0,1')
+		self.y_scale.setPlaceholderText(u'输入纵坐标轴最小和最大范围，以逗号分隔，如：0,1')
+		self.y_scale.setClearButtonEnabled(True)
 
 		# 横坐标轴标签名称
 		self.x_label = QLineEdit()
-		self.x_label.setText(u'The number of retrived samples')
+		self.x_label.setPlaceholderText(u'The number of retrived samples')
+		self.x_label.setClearButtonEnabled(True)
 
 		# 纵坐标轴标签名称
 		self.y_label = QLineEdit()
-		self.y_label.setText(u'precision @ 256 bins')
+		self.y_label.setPlaceholderText(u'precision @ 256 bins')
+		self.y_label.setClearButtonEnabled(True)
 
 		# 横坐标刻度
 		self.x_ticks = QLineEdit()
-		self.x_ticks.setText(u'输入刻度名称，以逗号分隔，如0,0.5,1.0')
+		self.x_ticks.setPlaceholderText(u'输入刻度名称，以逗号分隔，如0,0.5,1.0')
+		self.x_ticks.setClearButtonEnabled(True)
 
 		# 纵坐标刻度
 		self.y_ticks = QLineEdit()
-		self.y_ticks.setText(u'输入刻度名称，以逗号分隔，如0,0.5,1.0')
+		self.y_ticks.setPlaceholderText(u'输入刻度名称，以逗号分隔，如0,0.5,1.0')
+		self.y_ticks.setClearButtonEnabled(True)
 
 		# 线条风格预览按钮
 		self.lineview = QPushButton(u'预览')
@@ -139,6 +179,7 @@ class Config(QWidget):
 		line_layout.addRow(u'标记风格', self.markerstyle)
 		line_layout.addRow(u'标签名称', self.label)
 		line_layout.addRow(u'颜色配置', self.lineColor)
+		line_layout.addRow(u'标记密度', self.markDensity)
 		lineconfig_group.setLayout(line_layout)
 
 		# 坐标轴布局
@@ -165,8 +206,8 @@ class Config(QWidget):
 		# 按钮一：显示最终结果
 		# 按钮二：添加新的线条
 		# 按钮三：删除最近一条线条
-		self.showresult = QPushButton(u'显示结果')
-		self.showresult.setToolTip(u'结果展示！')
+		self.showresult = QPushButton(u'刷新视图')
+		self.showresult.setToolTip(u'刷新视图！')
 		self.showresult.clicked.connect(self.resultShow)
 		self.showresult.installEventFilter(self)
 
@@ -231,12 +272,16 @@ class Config(QWidget):
 		# self.view_face.setSizePolicy(self.view_face.sizeHint())
 		# 获取参数
 		self.formula = self.math.text()
+		if self.formula == "":
+			self.formula = self.math.placeholderText()
 		self.ls = self.linestyle.currentText()
 		self.mark = self.markerstyle.currentText()
 		self.lt = self.label.text()
+		if self.lt == "":
+			self.lt = self.label.placeholderText()
+		self.md = self.markDensity.value()
 
-		x0 = [0, 0.2, 0.4, 0.6, 0.8, 1.0]
-		y0 = [0, 0.2, 0.4, 0.6, 0.8, 1.0]
+		x0 = np.linspace(0, 1, 100)
 		try:
 			formula_str = self.formula.split('=')[1]
 			y0 = [eval(formula_str) for x in x0]
@@ -245,7 +290,7 @@ class Config(QWidget):
 		plt.cla()
 		fig = plt.figure()
 		ax = fig.add_subplot(1,1,1)
-		ax.plot(x0, y0, color=self.lc, ls=self.ls, marker=self.mark, label=self.lt)
+		ax.plot(x0, y0, color=self.lc, ls=self.ls, marker=self.mark, label=self.lt, markevery=self.md)
 		ax.legend(loc='best')
 		self.fig = fig
 		canvas = FigureCanvas(fig)
@@ -254,20 +299,32 @@ class Config(QWidget):
 
 	def resultShow(self):
 		try:
-			self.min_x, self.max_x = [float(k.strip()) for k in self.x_scale.text().strip().split(',')]
+			x_scale_text = self.x_scale.text()
+			if x_scale_text == "":
+				x_scale_text = self.x_scale.placeholderText()
+			self.min_x, self.max_x = [float(k.strip()) for k in x_scale_text.strip().split(',')]
 		except Exception as ex:
 			pass
 
 		try:
-			self.min_y, self.max_y = [float(k.strip()) for k in self.y_scale.text().strip().split(',')]
+			y_scale_text = self.y_scale.text()
+			if y_scale_text == "":
+				y_scale_text = self.y_scale.placeholderText()
+			self.min_y, self.max_y = [float(k.strip()) for k in y_scale_text.strip().split(',')]
 		except Exception as ex:
 			pass
 
 		self.x_text = self.x_label.text()
+		if self.x_text == "":
+			self.x_text = self.x_label.placeholderText()
 		self.y_text = self.y_label.text()
+		if self.y_text == "":
+			self.y_text = self.y_label.placeholderText()
 
 		try:
 			x_tick_str = self.x_ticks.text()
+			if x_tick_str == "":
+				x_tick_str = self.x_ticks.placeholderText()
 			self.x_label_list = [k.strip() for k in x_tick_str.strip().split(',')]
 			self.x_tick_list = np.linspace(self.min_x, self.max_x, len(self.x_label_list))
 		except Exception as ex:
@@ -275,6 +332,8 @@ class Config(QWidget):
 
 		try:
 			y_tick_str = self.y_ticks.text()
+			if y_tick_str == "":
+				y_tick_str = self.y_ticks.placeholderText()
 			self.y_label_list = [k.strip() for k in y_tick_str.strip().split(',')]
 			self.y_tick_list = np.linspace(self.min_y, self.max_y, len(self.y_label_list))
 		except Exception as ex:
@@ -285,7 +344,7 @@ class Config(QWidget):
 		for index in range(len(self.formula_list)):
 			x0 = np.linspace(self.min_x, self.max_x, 100)
 			y0 = [eval(self.formula_list[index].split('=')[1]) for x in x0]
-			ax.plot(x0, y0, color=self.lc_list[index], ls=self.ls_list[index], marker=self.mark_list[index], markevery=5, label=self.lt_list[index])
+			ax.plot(x0, y0, color=self.lc_list[index], ls=self.ls_list[index], marker=self.mark_list[index], markevery=self.mark_density_list[index], label=self.lt_list[index])
 		ax.legend(loc='best')
 		ax.set_xticks(self.x_tick_list)
 		ax.set_xticklabels(self.x_label_list)
@@ -303,11 +362,19 @@ class Config(QWidget):
 		try:
 			x = 1
 			temp = eval(self.math.text().split('=')[1])
-			self.formula_list.append(self.math.text())
+			math_txt = self.math.text()
+			if math_txt == "":
+				math_txt = self.math.placeholderText()
+			self.formula_list.append(math_txt)
 			self.ls_list.append(self.linestyle.currentText())
 			self.mark_list.append(self.markerstyle.currentText())
-			self.lt_list.append(self.label.text())
+			label_txt = self.label.text()
+			if label_txt == "":
+				label_txt = self.label.placeholderText()
+			self.lt_list.append(label_txt)
 			self.lc_list.append(self.lc)
+			self.mark_density_list.append(self.md)
+			self.resultShow()
 		except Exception as ex:
 			pass
 
@@ -320,13 +387,18 @@ class Config(QWidget):
 			self.mark_list.pop()
 			self.lt_list.pop()
 			self.lc_list.pop()
+			self.mark_density_list.pop()
+			self.resultShow()
 		except Exception as ex:
 			pass
 
 
 	def saveFig(self):
-		if self.fig:
-			plt.savefig('a.svg')
+		dialog = QFileDialog()
+		filename, filetype = dialog.getSaveFileName(self, "图像保存", "./result.png"\
+                  ,"All Files (*);;JPEG Files (*.jpg);;PNG Files (*.png);;SVG Files (*.svg)")
+		if filename!="" and self.fig:
+			plt.savefig(filename)
 
 
 	def eventFilter(self, obj, event):
@@ -334,11 +406,15 @@ class Config(QWidget):
 			if obj == self.lineColor:
 				self.statusBar.showMessage(u"点击此按钮将更改线条颜色！")
 				return False
+			elif obj == self.markDensity:
+				value = self.markDensity.value()
+				self.statusBar.showMessage(str(value))
+				return False
 			elif obj == self.lineview:
 				self.statusBar.showMessage(u"线条预览！")
 				return False
 			elif obj == self.showresult:
-				self.statusBar.showMessage(u'当前结果展示！')
+				self.statusBar.showMessage(u"刷新当前视图，重新显示图片！")
 				return False
 			elif obj == self.addline:
 				self.statusBar.showMessage(u'新增一条曲线！')
@@ -348,6 +424,14 @@ class Config(QWidget):
 				return False
 			elif obj == self.savefig:
 				self.statusBar.showMessage(u'将当前结果保存为高清图片！')
+				return False
+		elif event.type() == 2 or event.type() == 129:
+			if obj == self.markDensity:
+				value = self.markDensity.value()
+				self.statusBar.showMessage(str(value))
+				self.showLine()
+				return False
+			else:
 				return False
 		else:
 			return False
